389d18f61ae77ab8e7fa63f4d32204e9efb67456
Maciej <77107395+mkosnik1@users.noreply.github.com>
2026-09-08 20:54:31 +0200

    Fill final missing character feature descriptions

---
+++ js/mechanics-pl-final.js
@@ -0,0 +1,6 @@
+import { FEATURE_DETAILS } from "./mechanics-pl.js";
+
+Object.assign(FEATURE_DETAILS, {
+  "Ability Score Improvement": "Na tym poziomie rozwijasz postać. W tej konkretnej karcie wybrany wzrost cech albo atut jest już uwzględniony w wartościach i sekcji Atuty. Nie wybierasz go ponownie podczas gry.",
+  "Firbolg Magic": "Możesz raz na krótki albo długi odpoczynek użyć Wykrycia magii oraz Zmiany wyglądu bez wydawania komórki mocy. Zmiana wyglądu może sprawić, że wydajesz się do 3 stóp niższy lub wyższy niż normalnie. Jeśli masz własne komórki mocy, możesz również używać tych mocy normalnie."
+});
